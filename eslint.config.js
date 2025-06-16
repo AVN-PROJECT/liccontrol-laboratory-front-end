87f727d3e596e3c080380c78e59064af1cf678ee
AJlekCandr-proff <svznikov@yandex.ru>
2025-06-16 21:49:20 +0300

    style: Update eslint config.

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
+import vue from "eslint-plugin-vue";
 
+export default [
+  js.configs.recommended,
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,vue}"], languageOptions: { globals: globals.browser } },
-  pluginVue.configs["flat/essential"],
-]);
+  ...vue.configs["flat/essential"],
+
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  }
+];
