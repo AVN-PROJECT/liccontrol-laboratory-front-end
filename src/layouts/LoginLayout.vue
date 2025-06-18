318be7182ebd4cf78d2394ba8295053c9834095f
AJlekCandr-proff <svznikov@yandex.ru>
2025-06-18 22:54:21 +0300

    feat & fix: Create login view.

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="login-layout">
+    <div class="login-view">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
