2a4cff846831cbf0b7f39ec4ace9af0e424bf3fe
AJlekCandr-proff <svznikov@yandex.ru>
2025-06-19 00:30:24 +0300

    feat & fix: Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: '/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/styles/base/main" as *;',
+          api: 'modern-compiler',
+        },
+      },
+    },
+    define: { 'process.env': env },
+  };
 });
