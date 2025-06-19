f0e92bc190b1a3f00fb30b3639a92ea7f60bb69f
DYuMikheev <d.miheev@idaproject.com>
2025-06-19 13:21:56 +0300

    devops: донастроены линтеры

---
+++ src/components/ui/VButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <component
+        :is="tag"
+        class="VButton"
+        @click="$emit('click')"
+    >
+      <slot></slot>
+    </component>
+</template>
+
+<script setup>
+  defineProps({
+      tag: {
+          type: String,
+          default: 'button',
+          validator: value => ['button', 'a'].includes(value),
+      },
+
+    })
+</script>
+
+<style scoped></style>
