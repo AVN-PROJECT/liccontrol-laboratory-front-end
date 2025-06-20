0ed990abcea8aa4e35300cc6e775751de76ae95d
AJlekCandr-proff <svznikov@yandex.ru>
2025-06-20 11:30:52 +0300

    feat: Create simple header.

---
+++ src/components/modules/HeaderComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <div class="div1">
+      <img
+        src="@/assets/icons/logo.svg"
+        alt="logo-main.svg"
+      />
+      <h1>Лаборатория</h1>
+      <p>© All rights reserved</p>
+    </div>
+  </header>
+</template>
