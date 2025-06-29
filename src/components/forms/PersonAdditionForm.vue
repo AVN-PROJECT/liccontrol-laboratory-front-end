141d6562fd040dcda8b0297620785efc255e17a6
AJlekCandr-proff <svznikov@yandex.ru>
2025-06-29 22:11:40 +0300

    feat: Create PersonAdditionForm.vue

---
+++ src/components/forms/PersonAdditionForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="person__page-menu-addition-form">
+    <VInput
+      class="person__page-form-field"
+      placeholder="Имя Фамилия Отчество"
+    />
+    <VInput class="person__page-form-field" />
+    <VInput class="person__page-form-field" />
+    <VInput class="person__page-form-field" />
+    <VInput class="person__page-form-field" />
+  </div>
+</template>
+
+<script setup>
+  import VInput from '@/components/ui/VInput.vue';
+</script>
+
+<style scoped lang="scss">
+  /* styles go here */
+</style>
