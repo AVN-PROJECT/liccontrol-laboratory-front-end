1f46f99cb467c425bf9f64d559e477ac856492f6
AJlekCandr-proff <svznikov@yandex.ru>
2025-07-06 04:26:09 +0300

    feat: Create page instruction.

---
+++ src/views/InstructionView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="instruction__page-main"></div>
+</template>
+
+<script setup>
+  /* JS go here */
+</script>
+
+<style scoped lang="scss">
+  /* styles go here */
+</style>
