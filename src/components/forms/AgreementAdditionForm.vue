fdff967ca512f305dcbfda8c7ab316468333c047
AJlekCandr-proff <svznikov@yandex.ru>
2025-07-07 15:07:02 +0300

    feat: Create AgreementAdditionForm.vue.

---
+++ src/components/forms/AgreementAdditionForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="agreement__form--addition"></div>
+</template>
+
+<script setup>
+  // JS go here.
+</script>
+
+<style scoped lang="scss">
+  .agreement__form--addition {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    background: linear-gradient(45deg, rgb(143 200 155 / 30%) 0%, #f5f5f5 100%);
+    backdrop-filter: blur(5px);
+    gap: 1.5rem;
+  }
+</style>
