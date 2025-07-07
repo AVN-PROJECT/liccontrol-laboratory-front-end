24da04c1e32e2f5bd98c1267a2cbe0e4fe472ed9
AJlekCandr-proff <svznikov@yandex.ru>
2025-07-07 16:20:42 +0300

    feat: Update AgreementAdditionForm.vue. Definition base props & emits. Update minimal html-structure.

--- src/components/forms/AgreementAdditionForm.vue
+++ src/components/forms/AgreementAdditionForm.vue
@@ -1,9 +1,75 @@
 <template>
-  <div class="agreement__form--addition"></div>
+  <div class="agreement__form--addition">
+    <div class="form__buttons--close">
+      <VButton
+        class=""
+        click=""
+      >
+        <img
+          src=""
+          alt=""
+          class=""
+        />
+      </VButton>
+    </div>
+
+    <div class="form__columns">
+      <div class="form__column">
+        <label>
+          <VInput />
+        </label>
+      </div>
+      <div class="form__column">
+        <label>
+          <VInput />
+        </label>
+      </div>
+    </div>
+
+    <div class="form__buttons">
+      <VButton
+        class=""
+        @click="addAgreement"
+      >
+        Добавить договор
+      </VButton>
+    </div>
+  </div>
 </template>
 
 <script setup>
-  // JS go here.
+  // vue.
+  import { ref } from 'vue';
+  import VInput from '@/components/ui/VInput.vue';
+  import VButton from '@/components/ui/VButton.vue';
+  import resetForm from '@/helpers/forms/resetForm.js';
+
+  // constants
+
+  // const props = defineProps({
+  //   persons: {
+  //     default: () => [],
+  //     type: Array,
+  //   },
+  // });
+
+  const emit = defineEmits(['agreement-addition', 'close-modal']);
+
+  const newAgreement = ref({
+    agreement_number: '',
+    agreement_start_date: '',
+    agreement_final_date: '',
+    addresses: [],
+    persons: [],
+    equipments: [],
+    types_of_work: [],
+  });
+
+  const addAgreement = () => {
+    emit('agreement-addition', newAgreement.value);
+
+    resetForm(newAgreement.value);
+  };
 </script>
 
 <style scoped lang="scss">
